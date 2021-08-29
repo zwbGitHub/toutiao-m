add68959a9820dec743a36282b0ae1a877f7567a
张文博 <zwb960906@163.com>
2021-08-29 22:48:22 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>今日头条</h1>
+    <router-link>Home</router-link> |
+    <router-view />
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    lintOnSave: false,
+    devServer: {
+      // 首次编译成功直接打开浏览器
+      open: true,
+    },
+  }
+  
